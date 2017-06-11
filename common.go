66c235ad00ef7fcde3eeef75b8c00f188795b6b1
Jongseok Choi <js.choi.85@gmail.com>
2017-06-11 22:34:13 +0900

    config was added.

--- common.go
+++ common.go
@@ -1,10 +1,15 @@
 package nature
 
 import (
+	"io/ioutil"
 	"net/http"
 	"os"
+
+	"gitlab.artofthings.org/platform/ground/pkg/err"
+	yaml "gopkg.in/yaml.v2"
 )
 
+//WriteHTML sends a HTML file to a client
 func WriteHTML(w http.ResponseWriter, req *http.Request, filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -16,7 +21,25 @@ func WriteHTML(w http.ResponseWriter, req *http.Request, filename string) {
 	http.ServeContent(w, req, file.Name(), fi.ModTime(), file)
 }
 
+// Redirect redirects URL to a other URL within the same origin policy
 func Redirect(w http.ResponseWriter, r *http.Request, uri string) {
 	w.Header().Set("Location", uri)
 	w.WriteHeader(http.StatusFound)
+}
+
+// ConfigFromYaml loads configuration from YAML. The first argument is the
+// name of configuration file and another argument is the pointer type of
+// config variable.
+func ConfigFromYaml(filename string, config interface{}) interface{} {
+	c, e := ioutil.ReadFile(file)
+
+	err.Panic(e, "LoadConfig")
+
+	e = yaml.Unmarshal(c, r)
+
+	if e != nil {
+		err.Panic(e, "LoadConfig")
+	}
+
+	return r
 }
